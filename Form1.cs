b82052a9814edf6dbcbb1ee5d2cfcbd9f738d55a
Sam Williams <samuel.williams8@gmail.com>
2020-08-08 18:02:30 +1000

    Minor fixes.

--- Form1.cs
+++ Form1.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Runtime.InteropServices;
 using System.Threading;
 using System.Text.RegularExpressions;
-using System.Diagnostics;
 
 namespace TextToKeystrokes
 {
